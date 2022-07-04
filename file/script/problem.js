799e190f22d31eeed7dc6cf7ba5c050aa4609c83
Milmon <107737161+Molmin@users.noreply.github.com>
2022-07-04 21:03:01 +0800

    Update problem.js

--- file/script/problem.js
+++ file/script/problem.js
@@ -7,13 +7,13 @@ function gettags(texts,color){
 	return result;
 }
 function difficulty(x){
-	if(x==0)return "<span class='tag' style='background: #bfbfbf;' onclick='window.location.href=\"/problems?difficulty=0\";'>暂未评定</span>";
-	if(x==1)return "<span class='tag' style='background: #fe4c61;' onclick='window.location.href=\"/problems?difficulty=1\";'>入门</span>";
-	if(x==2)return "<span class='tag' style='background: #f39c11;' onclick='window.location.href=\"/problems?difficulty=2\";'>普及-</span>";
-	if(x==3)return "<span class='tag' style='background: #ffc116;' onclick='window.location.href=\"/problems?difficulty=3\";'>普及/提高-</span>";
-	if(x==4)return "<span class='tag' style='background: #52c41a;' onclick='window.location.href=\"/problems?difficulty=4\";'>普及+/提高</span>";
-	if(x==5)return "<span class='tag' style='background: #3498db;' onclick='window.location.href=\"/problems?difficulty=5\";'>提高+/省选-</span>";
-	if(x==6)return "<span class='tag' style='background: #9d3dcf;' onclick='window.location.href=\"/problems?difficulty=6\";'>省选/NOI-</span>";
-	if(x==7)return "<span class='tag' style='background: #11207e;' onclick='window.location.href=\"/problems?difficulty=7\";'>NOI/NOI+</span>";
-	if(x==8)return "<span class='tag' style='background: #052242;' onclick='window.location.href=\"/problems?difficulty=8\";'>CTSC/IOI</span>";
+	if(x==0)return "<span class='tag difficult-tag' style='background: #bfbfbf;' onclick='window.location.href=\"/problems?difficulty=0\";'>暂未评定</span>";
+	if(x==1)return "<span class='tag difficult-tag' style='background: #fe4c61;' onclick='window.location.href=\"/problems?difficulty=1\";'>入门</span>";
+	if(x==2)return "<span class='tag difficult-tag' style='background: #f39c11;' onclick='window.location.href=\"/problems?difficulty=2\";'>普及-</span>";
+	if(x==3)return "<span class='tag difficult-tag' style='background: #ffc116;' onclick='window.location.href=\"/problems?difficulty=3\";'>普及/提高-</span>";
+	if(x==4)return "<span class='tag difficult-tag' style='background: #52c41a;' onclick='window.location.href=\"/problems?difficulty=4\";'>普及+/提高</span>";
+	if(x==5)return "<span class='tag difficult-tag' style='background: #3498db;' onclick='window.location.href=\"/problems?difficulty=5\";'>提高+/省选-</span>";
+	if(x==6)return "<span class='tag difficult-tag' style='background: #9d3dcf;' onclick='window.location.href=\"/problems?difficulty=6\";'>省选/NOI-</span>";
+	if(x==7)return "<span class='tag difficult-tag' style='background: #11207e;' onclick='window.location.href=\"/problems?difficulty=7\";'>NOI/NOI+</span>";
+	if(x==8)return "<span class='tag difficult-tag' style='background: #052242;' onclick='window.location.href=\"/problems?difficulty=8\";'>CTSC/IOI</span>";
 }
